a9bc3230171a879d9d17f894a4ddee1003c3ccf8
voidrealms <bcairns@voidrealms.com>
2020-08-25 13:35:52 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <QCoreApplication>
+
+/*
+
+  What
+  Visitor design patterns
+
+  Description
+  Represent an operation to be performed on the elements of an object structure.
+
+  Why
+  And, you don't want to have to query the type of each node and cast the pointer to the correct type before performing the desired operation.
+
+  Example
+  Interfaces to the rescue!
+
+  Note - using interfaces simplifies this to the point that its almost not even the same pattern
+
+ */
+
+#include "cart.h"
+#include "shirt.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Cart cart;
+    Shirt shirt;
+    shirt.accept(&cart);
+
+
+    return a.exec();
+}
